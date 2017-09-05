379f65ecf51fff7295ba6ce5ffe22722944ae7da
dharmesh <dharmesh.patel@kiwiqa.com>
2017-09-05 12:22:47 +0530

    path issue

--- src/test/java/com/Framework/Init/SeleniumInit.java
+++ src/test/java/com/Framework/Init/SeleniumInit.java
@@ -156,7 +156,7 @@ public class SeleniumInit {
 			driver = new RemoteWebDriver(remote_grid, capability);
 		} else if (targetBrowser.contains("chrome") || targetBrowser.equalsIgnoreCase("chrome")) {
 			capability = DesiredCapabilities.chrome();
-			File driverpath = new File("/Resource/chromedriver");
+			File driverpath = new File("Resource/chromedriver");
 			String path1 = driverpath.getAbsolutePath();
 			System.out.println("+++++++++Chrome driver no path che :" +path1);
 			System.setProperty("webdriver.chrome.driver", path1);
